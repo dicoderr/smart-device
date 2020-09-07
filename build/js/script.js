a0cbb00e8fe8e7fa4a959a1079c9356c0cf734f4
Di Coderr <dicoderr@gmail.com>
2020-09-07 22:04:34 +0300

    Add hero (desktop)

--- build/js/script.js
+++ build/js/script.js
@@ -3,23 +3,23 @@
 var accordion = document.getElementsByClassName('accordion')[0];
 var accordionPanels = Array.from(accordion.getElementsByClassName('accordion__panel'));
 
-function toggle(panel) {
-  if (panel.classList.contains('active')) {
-    panel.classList.remove('active');
+function toggle(current) {
+  if (current.classList.contains('active')) {
+    current.classList.remove('active');
   } else {
-    accordionPanels.forEach(function (p) {
-      if (p === panel) {
-        p.classList.add('active');
+    accordionPanels.forEach(function (panel) {
+      if (panel === current) {
+        panel.classList.add('active');
       } else {
-        p.classList.remove('active');
+        panel.classList.remove('active');
       }
     });
   }
 }
 
-accordionPanels.forEach(function (p) {
-  var button = p.getElementsByClassName('accordion__button')[0];
+accordionPanels.forEach(function (panel) {
+  var button = panel.getElementsByClassName('accordion__button')[0];
   button.addEventListener('click', function () {
-    toggle(p);
+    toggle(panel);
   });
 });
