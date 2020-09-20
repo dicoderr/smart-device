b94890a6760dd53a4a0b40f09ba81e8c03e1c41e
Di Coderr <dicoderr@gmail.com>
2020-09-20 21:21:29 +0300

    Add header modal

--- build/js/script.js
+++ build/js/script.js
@@ -1,5 +1,39 @@
 'use strict';
 
+var modal = document.querySelector('.header__modal');
+var modalForm;
+var veil = document.querySelector('.header__modal .modal__veil');
+var callButton = document.querySelector('.navigation__recall');
+var modalOpened = false;
+
+function createModalForm() {
+  var form = document.getElementById('form').cloneNode(true);
+  form.id = 'form1';
+  form.classList.add('mobile');
+  form.querySelector('.form__title').textContent = 'Закажите звонок';
+  form.querySelector('.form__subtitle').textContent = 'Оставьте контакты, мы проконсультируем вас бесплатно в удобное время';
+  form.querySelector('button').textContent = 'Отправить';
+  var closeButton = document.createElement('button');
+  closeButton.classList.add('form__close');
+  closeButton.addEventListener('click', toggleModal);
+  form.append(closeButton);
+  return form;
+}
+
+function toggleModal() {
+  if (!modalForm) {
+    modalForm = createModalForm();
+    modal.querySelector('.modal__content').append(modalForm);
+  }
+  document.body.style.overflow = modalOpened ? 'visible' : 'hidden';
+  modal.classList.toggle('modal--visible');
+  modalOpened = !modalOpened;
+  return false;
+}
+
+callButton.addEventListener('click', toggleModal);
+veil.addEventListener('click', toggleModal);
+
 var accordion = document.getElementsByClassName('accordion')[0];
 var accordionPanels = Array.from(accordion.getElementsByClassName('accordion__panel'));
 
